e724191322ccc4c02c03bdb1654e9412c2d0b514
RenaldiHasudungan <89979042+RenaldiHasudungan@users.noreply.github.com>
2022-04-25 21:47:36 +0700

    Add files via upload

---
+++ Pertemuan8_71190491/app/src/androidTest/java/com/example/pertemuan8_71190491/main/java/com/example/pertemuan8_71190491/Fragment_Tiga.kt
@@ -0,0 +1,15 @@
+package com.example.pertemuan8_71190491
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class Fragment_Tiga: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = inflater.inflate(R.layout.fragmenttiga, container, false)
+}
